4938cbbe4d742e76452c612dce87dc6ccfab2e67
Sturjak <sturgeonjake52@gmail.com>
2017-12-05 13:41:39 +0000

    Working

--- code/system.py
+++ code/system.py
@@ -11,8 +11,11 @@ version: v1.0
 """
 import numpy as np
 import utils.utils as utils
+from scipy import ndimage
 import scipy.linalg
-
+import random
+import matplotlib.pyplot as plt
+import enchant
 
 def principal_components(X,num):
     covx = np.cov(X, rowvar=0)
@@ -112,14 +115,14 @@ def process_training_data(train_page_names):
 
     print('Extracting features from training data')
     bbox_size = get_bounding_box_size(images_train)
-    fvectors_train_full = images_to_feature_vectors(images_train, bbox_size)
+    images = remove_noise(images_train)
+    fvectors_train_full = images_to_feature_vectors(images, bbox_size)
 
     model_data = dict()
     model_data['labels_train'] = labels_train.tolist()
     model_data['bbox_size'] = bbox_size
-
     print('Reducing to 10 dimensions')
-    v = principal_components(fvectors_train_full, 10)
+    v = principal_components(fvectors_train_full, 11)[:,1:11]
     model_data['v'] = v.tolist()
     model_data['mean'] = np.mean(fvectors_train_full).tolist()
     model_data['fvectors_train'] = np.dot((fvectors_train_full - np.mean(fvectors_train_full)), v).tolist()
@@ -139,14 +142,84 @@ def load_test_page(page_name, model):
     """
     bbox_size = model['bbox_size']
     images_test = utils.load_char_images(page_name)
-    fvectors_test = images_to_feature_vectors(images_test, bbox_size)
+    n = remove_noise(images_test)
+    fvectors_test = images_to_feature_vectors(n, bbox_size)
     # Perform the dimensionality reduction.
     mean = np.array(model['mean'])
     v = np.array(model['v'])
     fvectors_test_reduced = np.dot((fvectors_test - mean), v)
     return fvectors_test_reduced
 
+def remove_noise(page):
+    n = []
+    for l in page:
+        image = ndimage.median_filter(l, 2)
+        for i, row in enumerate(image):
+            for j, px in enumerate(row):
+                if(px <= 152):
+                    image[i][j] = 0
+                else:
+                    image[i][j] = 255
+        n.append(image)
+#    plt.imshow(n[0],cmap='gray')
+#    plt.show()
+    return n
 
+#def correct_errors(page, labels, bboxes, model):
+#    """Dummy error correction. Returns labels unchanged.
+#
+#        parameters:
+#
+#        page - 2d array, each row is a feature vector to be classified
+#        labels - the output classification label for each feature vector
+#        bboxes - 2d array, each row gives the 4 bounding box coords of the character
+#        model - dictionary, stores the output of the training stage
+#        """
+#    words = []
+#    word = []
+#    for i in range(len(bboxes) - 1):
+#        word.append(labels[i])
+##        print(abs(bboxes[i][2]-bboxes[i+1][0]), abs(bboxes[i][3] - bboxes[i+1][1]))
+#        if(abs(bboxes[i][2]-bboxes[i+1][0]) >=7 or abs(bboxes[i][3] - bboxes[i+1][1]) >= 60):
+#            words.append(word)
+#            word = []
+#    word.append(labels[len(labels) - 1])
+#    words.append(word)
+#
+#    dictionary = enchant.Dict("en_GB")
+#    new_words = []
+#    l = [',',"'",'.','!']
+#    for word in words:
+#        w = ''.join(word)
+#        end = []
+#        if w[len(w) - 1] in l:
+##            print(w, w[len(w) - 1])
+#            end = w[len(w) - 1]
+#            w = w[:len(w) - 1]
+##            print(w)
+#        last = ''
+#        if(len(w) > 0 and dictionary.check(w) == False):
+#            s = dictionary.suggest(w)
+#            sugg = [x for x in s if len(x) == len(w)]
+#            if(len(sugg) == 0):
+##                print(w, sugg, w)
+#                new_words.append(list(w))
+#            else:
+##                print(w, sugg, sugg[0])
+#                new_words.append(list(sugg[0]))
+#        else:
+#            new_words.append(list(w))
+#        if(len(end) > 0):
+#            new_words.append(list(end))
+#        print(new_words[len(new_words)-1])
+#
+#    letters = []
+#    for word in new_words:
+#        for char in word:
+#            letters.append(char)
+##    print(np.array(letters).shape)
+##    print(labels.shape)
+#    return np.array(letters)
 def classify_page(page, model):
     """Dummy classifier. Always returns first label.
 
@@ -157,10 +230,10 @@ def classify_page(page, model):
     """
     fvectors_train = np.array(model['fvectors_train'])
     labels_train = np.array(model['labels_train'])
+    print("classify")
+    return classify(fvectors_train, labels_train, page, int(page.shape[0]/page.shape[1]))
 
-    return classify(fvectors_train, np.expand_dims(labels_train, axis=0), page)
-
-def classify(train, train_labels, test, features=None):
+def classify(train, train_labels, test, k, features=None):
     """Perform nearest neighbour classification."""
     
     # Use all feature is no feature parameter has been supplied
@@ -170,14 +243,57 @@ def classify(train, train_labels, test, features=None):
     # Select the desired features from the training and test data
     train = train[:, features]
     test = test[:, features]
+    
+    nt = np.unique(train, axis=0)
+
+#    nc = np.copy(train)
+#    for k,x in enumerate(nc):
+#        for i in range(1,2):
+#            rand = random.randrange(5)
+#            nx = np.copy(x)
+#            nx = nx + rand
+#            train = np.append(train, np.expand_dims(nx, axis=0), axis=0)
+#            train_labels = np.append(train_labels, train_labels[k])
 
 
     # Super compact implementation of nearest neighbour
-    x= np.dot(test, train.transpose())
+    x = np.dot(test, train.transpose())
     modtest=np.sqrt(np.sum(test * test, axis=1))
     modtrain=np.sqrt(np.sum(train * train, axis=1))
-    dist = x / np.outer(modtest, modtrain.transpose()) # cosine distance
-    nearest=np.argmax(dist, axis=1)
-    mdist=np.max(dist, axis=1)
+    x /= np.outer(modtest, modtrain.transpose()) # cosine distance
+    dist = x
+    nearest = np.argmax(dist, axis=1)
 
-    return train_labels[0, nearest]
+    kNearest = np.argsort(dist, axis=1)[:, ::-1][:,:k]
+    labels = []
+    w = [1/x for x in range(1,k+1)]
+    for row in kNearest:
+        a, b = np.unique(train_labels[row], return_index=True)
+        t = np.array(list(zip(train_labels[row], w)))
+        unique_weights = []
+        for unique in a:
+            ts = t[t[:,0] == unique, :]
+            weights = ts[:, 1].astype(float)
+            unique_weights.append(np.sum(weights, axis=0))
+        i = np.argmax(np.array(unique_weights))
+        labels.append(row[b[i]])
+#        label = []
+#        if(len(a) == 1):
+#            label = a[0]
+#        else:
+#        totals = [0] * len(a)
+#        for i, letter in enumerate(a):
+#            for y in t:
+#                if(y[0] == letter):
+#                    totals[i] += y[1]
+#        totals = np.array(totals)
+#        label = a[np.argmax(totals)]
+
+#        labels = np.append(labels, train_labels[0,:].tolist().index(label))
+#    labels = labels.astype(int)
+    return train_labels[labels]
+
+
+
+
+
